1c9e879168532581bd5cc9461b8685468a3782ca
Ihago Santos <ihagog12@gmail.com>
2023-04-24 21:25:41 -0300

    feat: modify lambda function

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,6 +3,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
     return new Promise(async (resolve) => {
         const method = event.httpMethod
+        const lambdaRequestId = context.awsRequestId
+        const apiRequestId = event.requestContext.requestId
+
+        console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+
         if(event.resource === '/products') {
             if( method === 'GET' ){
                 console.log('GET - /products')
